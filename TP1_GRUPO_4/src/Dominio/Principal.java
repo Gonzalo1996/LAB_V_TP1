8035bb74999d4121217222192009e1c97190b46f
Gonzalo1996 <gonald96@gmail.com>
2021-04-01 22:18:38 -0300

    Creación de las clases: Actores, GeneroTeatro, Teatro. E instancia de el main de las mismas.

--- TP1_GRUPO_4/src/Dominio/Principal.java
+++ TP1_GRUPO_4/src/Dominio/Principal.java
@@ -3,6 +3,10 @@ package Dominio;
 import Recital.Banda;
 import Recital.Genero;
 import Recital.Recital;
+import Teatro.Actores;
+import Teatro.GeneroTeatro;
+import Teatro.Teatro;
+
 
 public class Principal {
 
@@ -12,5 +16,18 @@ public class Principal {
 		Genero g = new Genero(1, "Rock");
 		Recital recital = new Recital(b,g,"La renga");
 		System.out.println(recital.toString());
+		
+		GeneroTeatro generoA = new GeneroTeatro(1, "terror");
+		Actores actorA = new Actores(1, "Facundo Arana");
+		
+		Teatro teatroA = new Teatro("Gran rex", generoA, actorA, actorA, actorA);
+		Teatro teatroB = new Teatro("Luna Park", generoA, actorA, actorA, actorA);		
+		
+		System.out.println(teatroA.toString());
+		System.out.println(teatroB.toString());
+		
+		
+		
+		
 	}
 }
---
+++ TP1_GRUPO_4/src/Teatro/Teatro.java
@@ -0,0 +1,81 @@
+package Teatro;
+
+public class Teatro {
+	
+	private int id;
+	private String nombre;
+	private GeneroTeatro genero;
+	private Actores actorA;
+	private Actores actorB;
+	private Actores actorC;
+	private static int contId = 0;
+	
+/*<------------CONSTRUCTORES-------------------->*/	
+	
+	public Teatro() {
+		contId++;
+		this.id = contId;
+		this.nombre = "Sin nombre";
+	}
+	
+	public Teatro(String nombre, GeneroTeatro genero, Actores actorA, Actores actorB, Actores actorC) {
+		contId++;
+		this.id = contId;
+		this.nombre = nombre;
+		this.genero = genero;
+		this.actorA = actorA;
+		this.actorB = actorB;
+		this.actorC = actorC;
+	}
+	
+/*<------------GETTERS AND SETTERS-------------->*/
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public GeneroTeatro getGenero() {
+		return genero;
+	}
+	public void setGenero(GeneroTeatro genero) {
+		this.genero = genero;
+	}
+
+	public Actores getActorA() {
+		return actorA;
+	}
+
+	public void setActorA(Actores actorA) {
+		this.actorA = actorA;
+	}
+
+	public Actores getActorB() {
+		return actorB;
+	}
+
+	public void setActorB(Actores actorB) {
+		this.actorB = actorB;
+	}
+
+	public Actores getActorC() {
+		return actorC;
+	}
+
+	public void setActorC(Actores actorC) {
+		this.actorC = actorC;
+	}
+
+	@Override
+	public String toString() {
+		return "ID teatro: " + id + ". Nombre: " + nombre + ". Genero: " + genero.getNombre() + ". Actores: " + actorA.getNombre() + "," + actorB.getNombre() + 
+				"," + actorC.getNombre() + ".";
+	}	
+}
